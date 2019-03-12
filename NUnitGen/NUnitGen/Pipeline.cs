07f4d8d9aa080c7528b43d88add52ecca71f65a0
ActualDennis <actualdennis@gmail.com>
2019-03-12 11:32:36 +0300

    [NUnitGen] Added Nunit tests generator.

---
+++ NUnitGen/NUnitGen/Pipeline.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitGen {
+    public class PipeLine {
+        public PipeLine(int maxFilesToLoad, int maxTasksExecuted, int maxFilesToWrite)
+        {
+            this.maxFilesToLoad = maxFilesToLoad;
+            this.maxFilesToWrite = maxFilesToWrite;
+            this.maxTasksExecuted = maxTasksExecuted;
+        }
+
+        private int maxFilesToLoad { get; set; }
+
+        private int maxTasksExecuted { get; set; }
+
+        private int maxFilesToWrite { get; set; }
+
+
+    }
+}
---
+++ NUnitGen/NUnitGen/ViewModels/ApplicationViewModel.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Win32;
+using NUnitGen.Helpers;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace NUnitGen {
+
+    /// <summary>
+    /// ViewModel for storing information about current chat and messages in chat
+    /// </summary>
+    public class ApplicationViewModel : BaseViewModel {
+
+        public ApplicationViewModel()
+        {
+            
+        }
+
+        public string MaxFilesToWrite { get; set; }
+
+        public string MaxTasksExecuted { get; set; }
+
+        public string MaxFilesToLoad { get; set; }
+
+        private List<string> LoadedClasses { get; set; }
+
+        public ICommand ExecuteCommand => new RelayCommand(() => Execute(null), null);
+
+        public ICommand AddClassCommand => new RelayCommand(() => AddClass(null), null);
+
+
+
+        private void AddClass(object p)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "C# class file(*.cs)|*.cs";
+            var result = dialog.ShowDialog();
+
+            if (result == null || result == false)
+                return;
+
+            LoadedClasses.Add(dialog.FileName);
+        }
+
+        private void Execute(object p)
+        {
+            var dialog = new SaveFileDialog();
+            dialog.Filter = "C# NUnit test file(*.cs)|*.cs";
+            var result = dialog.ShowDialog();
+
+            if (result == null || result == false)
+                return;
+
+            var destination = dialog.FileName;
+        }
+
+    }
+
+
+
+
+
+
+}
